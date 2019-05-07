6a73eb76d3db3966215c0be7b70919986127e7d6
Vladyslav <>
2019-05-07 14:49:24 +0300

    Commit title here

---
+++ Extensions/File.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  Extensions
+//
+//  Created by Vladyslav Tsykhmistro on 5/7/19.
+//  Copyright © 2019 Vladyslav Tsykhmistro. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var isEmail: Bool {
+        
+        let pattern = "(?:[a-zA-Z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%\\&'*+/=?\\^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        
+        let predicate = NSPredicate(format: "SELF MATCHES %@", pattern)
+        let result = predicate.evaluate(with: self)
+        return result
+        
+    }
+}
